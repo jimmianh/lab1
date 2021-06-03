7d46f87a9766f0afad9bb1a97f1dfe74dcfd60c5
jimmianh <73330878+jimmianh@users.noreply.github.com>
2021-06-03 11:59:33 +0700

    nopbailab1

---
+++ Lab01/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab01
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //đây là bài 1 nếu muốn chạy hãy tắt Comment
+            // String name;
+            // String address;
+            // String phone;
+            // Console.WriteLine("Xin moi nhap thong tin");
+            // Console.WriteLine("Nhap ten:");
+            // name = Console.ReadLine();
+            // Console.WriteLine("Nhap dia chi");
+            // address = Console.ReadLine();
+            // Console.WriteLine("Nhap so dien thoai");
+            // phone = Console.ReadLine();
+            // string s = String.Format("ten nguoi dung: {0}, dia chi: {1}, so dien thoai: {2}", name, address, phone);
+            // Console.WriteLine(s);
+            
+            // đây là bài 2 nè chạy thì cũng tắt comment đi nha 
+            // int a, b, c;
+            // Console.WriteLine("Vui lòng nhập 3 số nguyên để tìm ra số lớn nhất");
+            // Console.WriteLine("//");
+            // Console.WriteLine("Nhập số nguyên thứ nhất");
+            // a = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Nhập số nguyên thứ hai");
+            // b = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine("Nhập số nguyên thứ ba");
+            // c = Convert.ToInt32(Console.ReadLine());
+            //đoạn này là vòng lặp để tìm ra số lớn nhất và in ra 
+            // if (a > b && a > c)
+            // {
+            //     Console.WriteLine("Số lớn nhất là số thứ 1");
+            // }
+            // if (b > a && b > c)
+            // {
+            //     Console.WriteLine("Số lớn nhất là số thứ 2");
+            // }
+            // if (c > b && c > a)
+            // {
+            //     Console.WriteLine("Số lớn nhất là số thứ 3");
+            // }
+            
+            //bài 3
+            // string[] day = {"DayNull", "mon", "tue", "wed", "thus", "fri", "sat","sun"};
+            // Console.WriteLine("Nhap 1 số (1-7)");
+            // int i = Convert.ToInt32(Console.ReadLine());
+            // if (0 < i && i< 8)
+            // {
+            //     Console.WriteLine(day[i]);
+            // }
+            // else
+            // {
+            //     Console.WriteLine("tao bao chi nhap tu 1 den 7 thoi co ma");
+            // }
+            
+            // bài 4
+            // Console.WriteLine("Nhap 1 so de nhan lai duoc 9 boi so dau tien cua nó");
+            // int a = Convert.ToInt32(Console.ReadLine());
+            // String s = String.Format("9 boi so dau tien cua {0} la:", a);
+            // Console.WriteLine(s);
+            // for (int i = 1; i < 10; i++)
+            // {
+            //     Console.WriteLine(a*i);
+            // }
+        }
+    }
+}
